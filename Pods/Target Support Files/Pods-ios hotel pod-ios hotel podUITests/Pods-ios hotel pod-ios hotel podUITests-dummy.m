88921eabf99d6aa9b35208ea0fd71ac951f29077
hamzhya <hamzhyatinnov@gmail.com>
2020-12-03 15:50:29 +0700

    initial commit

---
+++ Pods/Target Support Files/Pods-ios hotel pod-ios hotel podUITests/Pods-ios hotel pod-ios hotel podUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_hotel_pod_ios_hotel_podUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ios_hotel_pod_ios_hotel_podUITests
+@end
---
+++ ios hotel pod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios hotel pod
+//
+//  Created by Hamzhya on 03/12/20.
+//  Copyright © 2020 Hamzhya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
